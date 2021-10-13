57d96d90cbea8363e7959d9534207a37b48b2566
dmaglobalia <dma.globaliasoft@gmail.com>
2021-10-13 14:24:01 +0530

    rename procfile

--- whether/settings.py
+++ whether/settings.py
@@ -132,3 +132,12 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/static/',
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+import dj_database_url
+import django_heroku
+
+django_heroku.settings(locals())
