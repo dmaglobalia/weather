e3c34ac62522a43dcb3c73184061db04a4740c6f
dmaglobalia <dma.globaliasoft@gmail.com>
2021-10-13 10:56:28 +0530

    intial commit before hosting

--- whether/urls.py
+++ whether/urls.py
@@ -16,11 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+
+from django.conf.urls import url
+from django.views.static import serve
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
-
 
 handler404 = 'home.views.error_404_view'
 handler500 = 'home.views.handler500'
